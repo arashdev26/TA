7c0d9bb349eb6f8f7be84db1a35f18ceec91cf47
Arash Asghari <arashasghari@Arashs-MBP.Home>
2024-06-05 20:02:50 +0330

    first collection  image uploaded

---
+++ app/(dashboard)/collections/new/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CollectionForm from '@/components/collections/collectionForm'
+
+const CreateCollection = () => {
+  return <CollectionForm />
+}
+
+export default CreateCollection
---
+++ components/collections/CollectionForm.tsx
@@ -0,0 +1,116 @@
+'use client'
+
+import React from 'react'
+import { z } from 'zod'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+import { Separator } from '../ui/separator'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '../ui/textarea'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import ImageUpload from '../custom ui/ImageUpload'
+
+const formSchema = z.object({
+  title: z.string().min(2).max(20),
+  description: z.string().min(2).max(500).trim(),
+  image: z.string(),
+})
+
+const CollectionForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+      image: '',
+    },
+  })
+
+  // 2. Define a submit handler.
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <div className="p-10">
+      <p className="text-heading2-bold">Create Collection</p>
+      <Separator className="my-4 bg-grey-1 mt-4 mb-7" />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8"
+        >
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Title"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    rows={5}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Image</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange('')}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
+
+export default CollectionForm
