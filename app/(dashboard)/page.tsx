a6638a5f4ec09babab5961e4c49f66d5b1ed8cb3
Arash Asghari <arashasghari@Arashs-MacBook-Pro.local>
2024-06-08 15:45:00 +0330

    end

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,19 +1,19 @@
-import SalesChart from "@/components/custom ui/SalesChart";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
+import SalesChart from '@/components/custom ui/SalesChart'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
 import {
   getSalesPerMonth,
   getTotalCustomers,
   getTotalSales,
-} from "@/lib/actions/actions";
-import { CircleDollarSign, ShoppingBag, UserRound } from "lucide-react";
+} from '@/lib/actions/actions'
+import { CircleDollarSign, ShoppingBag, UserRound } from 'lucide-react'
 
 export default async function Home() {
-  const totalRevenue = await getTotalSales().then((data) => data.totalRevenue);
-  const totalOrders = await getTotalSales().then((data) => data.totalOrders);
-  const totalCustomers = await getTotalCustomers();
+  const totalRevenue = await getTotalSales().then((data) => data.totalRevenue)
+  const totalOrders = await getTotalSales().then((data) => data.totalOrders)
+  const totalCustomers = await getTotalCustomers()
 
-  const graphData = await getSalesPerMonth();
+  const graphData = await getSalesPerMonth()
 
   return (
     <div className="px-8 py-10">
@@ -61,5 +61,6 @@ export default async function Home() {
         </CardContent>
       </Card>
     </div>
-  );
+  )
 }
+export const dynamic = 'force-dynamic'
